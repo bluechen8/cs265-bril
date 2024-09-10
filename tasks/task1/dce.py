b428f19f2595f1070a51201ca1df7e1fa21d176b
Boru Chen <eebrchen@gmail.com>
2024-09-09 17:43:46 -0700

    iterate global dead code elimination

--- tasks/task1/dce.py
+++ tasks/task1/dce.py
@@ -1,8 +1,8 @@
 import json
 import sys
 
-if __name__ == "__main__":
-    prog = json.load(sys.stdin)
+# do a single pass for global dead code elimination
+def g_dce_single(prog):
     # Arg set and dest set
     arg_set = set()
     dest_set = set()
@@ -18,5 +18,17 @@ if __name__ == "__main__":
     # Remove unused variables
     for fn in prog["functions"]:
         fn["instrs"] = [instr for instr in fn["instrs"] if instr.get("dest") not in unused_set]
+    # return the size of unused_set
+    return len(unused_set)
+
+# passes for global dead code elimination
+def g_dce(prog):
+    # call g_dce_single until no more unused variables
+    while g_dce_single(prog):
+        pass
+
+if __name__ == "__main__":
+    prog = json.load(sys.stdin)
+    g_dce(prog)
     # Output the program
     json.dump(prog, sys.stdout, indent=2)
