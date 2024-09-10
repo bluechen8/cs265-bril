87a9b5b71dbf86471715a070422e845f244c0741
Boru Chen <eebrchen@gmail.com>
2024-09-09 20:55:00 -0700

    analyze program per func

--- tasks/task1/dce.py
+++ tasks/task1/dce.py
@@ -2,33 +2,33 @@ import json
 import sys
 
 # do a single pass for global dead code elimination
-def g_dce_single(prog):
+def g_dce_single(fn):
     # Arg set and dest set
     arg_set = set()
     dest_set = set()
-    # Skim the program for args and dests
-    for fn in prog["functions"]:
-        for instr in fn["instrs"]:
-            if "args" in instr:
-                arg_set.update(instr["args"])
-            if "dest" in instr:
-                dest_set.add(instr["dest"])
+    # Skim the function for args and dests
+    for instr in fn["instrs"]:
+        if "args" in instr:
+            arg_set.update(instr["args"])
+        if "dest" in instr:
+            dest_set.add(instr["dest"])
     # Variable not used
     unused_set = dest_set - arg_set
     # Remove unused variables
-    for fn in prog["functions"]:
-        fn["instrs"] = [instr for instr in fn["instrs"] if instr.get("dest") not in unused_set]
+    fn["instrs"] = [instr for instr in fn["instrs"] if instr.get("dest") not in unused_set]
     # return the size of unused_set
     return len(unused_set)
 
 # passes for global dead code elimination
-def g_dce(prog):
+def g_dce(fn):
     # call g_dce_single until no more unused variables
-    while g_dce_single(prog):
+    while g_dce_single(fn):
         pass
 
 if __name__ == "__main__":
     prog = json.load(sys.stdin)
-    g_dce(prog)
+    # Analyze the program per-func
+    for fn in prog["functions"]:
+        g_dce(fn)
     # Output the program
     json.dump(prog, sys.stdout, indent=2)
